78653dbfbe90d90d7fd16361fe4549443db009fd
Ayman <aymanm95@gmail.com>
2015-11-04 16:19:19 +0000

    Parsing functions added (not all)

--- src/parser.go
+++ src/parser.go
@@ -53,6 +53,179 @@ func (p *parser) parseProgram() (bool, string) {
 	return false, ""
 }
 
+// Maps error messages with the associated terminal
+func (p *parser) terminalErrorMessages(token tokenType) (bool, []string) {
+	return false, []string{""}
+}
+
 /* TERMINALS */
+
+func (p *parser) parseBegin() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseEnd() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseIs() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseSkip() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseRead() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseFree() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseReturn() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseExit() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parsePrint() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parsePrintln() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseIf() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseThen() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseElse() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseFi() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseWhile() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseDo() (bool, string) {
+	return false, ""
+}
+func (p *parser) parseDone() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseStat() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseNewpair() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseFst() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseSnd() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseInt() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseBool() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseChar() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseString() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parsePair() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseTrue() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseFalse() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseNull() (bool, string) {
+	return false, ""
+}
+
+// '#' is a comment
+func (p *parser) parseComment() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseLBracket() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseRBracket() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseComma() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseEquals() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseSemiColon() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseLSquareBracket() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseRSquareBracket() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseExclamation() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseMinus() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseLen() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseOrd() (bool, string) {
+	return false, ""
+}
+
+func (p *parser) parseChr() (bool, string) {
+	return false, ""
+}
 
 /* ---------------------------------------------------------------------------*/
