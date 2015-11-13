2a6e267bbdbbe38fec9fcf81cf14ef4ec66b8ed7
Nana <na1814@ic.ac.uk>
2015-11-13 00:26:16 +0000

    Updated compiler.go with oli's file

--- src/compiler.go
+++ src/compiler.go
@@ -6,22 +6,29 @@ import (
 	"io/ioutil"
 	"os"
 
-	lexer "github.com/OliWheeler/wacc_19/src/lexer"
+	"github.com/nanaasiedu/wacc_19/src/parse"
 )
 
 func main() {
-	var tokens []lexer.Item
-	b, err := ioutil.ReadFile("../../../../../wacc_examples/valid/function/nested_functions/fibonacciFullRec.wacc")
+	var tokens []parse.Token
+	//	b, err := ioutil.ReadFile("wacc_examples/valid/variables/capCharDeclaration.wacc")
+	//	b, err := ioutil.ReadFile("wacc_examples/valid/function/nested_functions/fibonacciFullRec.wacc")
+	b, err := ioutil.ReadFile("wacc_examples/invalid/syntaxErr/expressions/missingOperand1.wacc")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	s := string(b)
-	_ = "breakpoint"
-	lex := lexer.Lex("Something", s)
-	for item := range lex.Items {
+	lexer := parse.Lex("Something", s)
+	for item := range lexer.Items {
 		tokens = append(tokens, item)
 	}
 	fmt.Println(tokens)
+	fmt.Println("\n------ Completed Lexing ------\n")
+	parser := lexer.ConstructParser(tokens)
+	passed, errs := parser.Parse()
+	fmt.Println("Parsing Successful:", passed)
+	fmt.Println("Parsing errors:", errs)
+	fmt.Println("\n------ Completed Parsing ------\n")
 
 }
