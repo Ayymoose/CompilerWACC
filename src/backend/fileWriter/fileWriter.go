e7e5abbcef3c8dbcbec11ef18b48a76459002638
Henryk Hadass <hh2214@ic.ac.uk>
2015-12-01 21:55:42 +0000

    Updated file writer

--- src/backend/fileWriter/fileWriter.go
+++ src/backend/fileWriter/fileWriter.go
@@ -8,10 +8,23 @@ func check(e error) {
 	}
 }
 
-func writeToFile(input string) {
-	f, err := os.Create("/fileWriter/original.s")
+// File struct which contains an array of ARM instuction set strings
+type File struct {
+	code []string
+}
+
+// Appends a string to the code slice in the File struct
+func (f File) appendStringToFile(input string) {
+	f.code = append(f.code, input)
+}
+
+// Writes all the strings contained in the File struct to the file
+func (f File) writeToFile(input string) {
+	file, err := os.Create("/fileWriter/original.s")
 	check(err)
-	defer f.Close()
-	n3, err := f.WriteString(input + "\n")
-	f.Sync()
+	defer file.Close()
+	for _, str := range f.code {
+		n3, err := file.WriteString(str)
+	}
+	file.Sync()
 }
