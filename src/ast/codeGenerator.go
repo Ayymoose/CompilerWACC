407bbd4cd3cc1dd11e0c7b327b9274809fe3f250
Nana <na1814@ic.ac.uk>
2015-12-03 00:51:46 +0000

    Added many comments to codeGenerator

--- src/ast/codeGenerator.go
+++ src/ast/codeGenerator.go
@@ -5,30 +5,38 @@ import (
 	"strconv"
 )
 
+// CodeGenerator: Using a constructor that provides an address of the place holder
+// for the list of assembly instructions for the program (And the AST with the symbol table),
+// the code generator can use the "GenerateCode" function to fill the assembly list
+// instrs with the assembly produced by traversing the tree
 type CodeGenerator struct {
-	root        *Program
-	instrs      *ARMList
-	msgInstrs   ARMList
-	symTable    SymbolTable
-	globalStack *scopeData
-	currStack   *scopeData
+	root        *Program          // Root of the AST
+	instrs      *ARMList          // List of assembly instructions for the program
+	msgInstrs   ARMList           // List of assembly instructions to create msg labels
+	symTable    SymbolTable       // Used to map variable identifiers to their values
+	globalStack *scopeData        // Stack data for the global scope
+	currStack   *scopeData        // Stack data for the current scope
+	msgMap      map[string]string // Maps string values to their msg labels
 }
 
+// Constructor for the code generator.
 func ConstructCodeGenerator(cRoot *Program, cInstrs *ARMList, cSymTable SymbolTable) CodeGenerator {
 	cg := CodeGenerator{root: cRoot, instrs: cInstrs, msgInstrs: ARMList{},
 		symTable: cSymTable, globalStack: &scopeData{}}
+	// The program starts off with the current scope as the global scope
 	cg.currStack = cg.globalStack
 	return cg
 }
 
+// Provides information about the stack in relation to a specific scope
 type scopeData struct {
-	currP       int // the current position of the pointer to the stack
-	size        int // size of the stack space in bytes
-	parentScope *scopeData
+	currP       int        // the current position of the pointer to the stack
+	size        int        // size of the variable stack scope space in bytes
+	parentScope *scopeData // Address of the parent scope
 }
 
 // Decreases current pointer to the stack by n
-// Returns new currP as a string
+// Returns new currP as a string (Does not have to be used)
 func (cg CodeGenerator) subCurrP(n int) string {
 	cg.currStack.currP = cg.currStack.currP - n
 	return strconv.Itoa(cg.currStack.currP)
@@ -36,6 +44,7 @@ func (cg CodeGenerator) subCurrP(n int) string {
 
 // Using the ARMList pointer provided in the constructor,
 // this function will fill the slice with an array of assembly instructions
+// based on the provided AST
 func (cg CodeGenerator) GenerateCode() {
 	cg.cgVisitProgram(cg.root)
 }
