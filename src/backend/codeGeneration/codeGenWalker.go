e1ec747e919892310ca1e04ee84ba46045ffb6c7
Nana <na1814@ic.ac.uk>
2015-12-07 17:40:31 +0000

    IO break (Binop)

--- src/backend/codeGeneration/codeGenWalker.go
+++ src/backend/codeGeneration/codeGenWalker.go
@@ -277,9 +277,9 @@ func (cg CodeGenerator) evalRHS(t Evaluation, srcReg string) {
 		cg.evalArrayElem(t, srcReg, "r5")
 	case Unop:
 		cg.cgVisitUnopExpr(t.(Unop))
-	/*case Binop:
+	case Binop:
 		cg.cgVisitBinopExpr(t.(Binop))
-	case NewPair:
+	/*case NewPair:
 		// First allocate memory to store two addresses (8-bytes)
 		cg.CfunctionCall("malloc", strconv.Itoa(ADDR_SIZE*2))
 		cg.evalPair(t.(NewPair).FstExpr, t.(NewPair).SndExpr, "r5", srcReg)
