d10186df6ffb96e625cfcdc6ad38f556a4c9ddb4
Henryk Hadass <hh2214@ic.ac.uk>
2015-12-07 21:11:51 +0000

    Fixed LTE error in Binop

--- src/backend/codeGeneration/codeGenWalker.go
+++ src/backend/codeGeneration/codeGenWalker.go
@@ -939,10 +939,14 @@ func (cg CodeGenerator) cgVisitBinopExpr(node Binop) {
 		appendAssembly(cg.currInstrs(), "AND r4, r4, r5", 1, 1)
 	case OR:
 		appendAssembly(cg.currInstrs(), "ORR r4, r4, r5", 1, 1)
-	case LT, LTE, GT, GTE:
+	case LT, GT, GTE:
 		appendAssembly(cg.currInstrs(), "CMP r4, r5", 1, 1)
 		appendAssembly(cg.currInstrs(), "MOVLT r4, #1", 1, 1)
 		appendAssembly(cg.currInstrs(), "MOVGE r4, #0", 1, 1)
+	case LTE:
+		appendAssembly(cg.currInstrs(), "CMP r4, r5", 1, 1)
+		appendAssembly(cg.currInstrs(), "MOVLE r4, #1", 1, 1)
+		appendAssembly(cg.currInstrs(), "MOVGT r4, #0", 1, 1)
 	case EQ, NEQ:
 		appendAssembly(cg.currInstrs(), "CMP r4, r5", 1, 1)
 		appendAssembly(cg.currInstrs(), "MOVEQ r4, #1", 1, 1)
