248b8c67346837cd31f7bc05bfd4f3794e9622c7
Nana <na1814@ic.ac.uk>
2015-12-08 03:13:21 +0000

    Fixed B instr

--- src/backend/codeGeneration/codeGenWalker.go
+++ src/backend/codeGeneration/codeGenWalker.go
@@ -777,7 +777,7 @@ func (cg *CodeGenerator) cgVisitIfStat(node If) {
 
 	cg.cgVisitScopeStat(Scope{StatList: node.ThenStat})
 
-	appendAssembly(cg.currInstrs(), "B "+sndLabel+":", 1, 1)
+	appendAssembly(cg.currInstrs(), "B "+sndLabel, 1, 1)
 	appendAssembly(cg.currInstrs(), fstLabel+":", 0, 1)
 
 	cg.cgVisitScopeStat(Scope{StatList: node.ElseStat})
@@ -789,7 +789,7 @@ func (cg *CodeGenerator) cgVisitIfStat(node If) {
 func (cg *CodeGenerator) cgVisitWhileStat(node While) {
 	fstLabel, sndLabel := cg.getNewLabel(), cg.getNewLabel()
 
-	appendAssembly(cg.currInstrs(), "B "+sndLabel+":", 1, 1)
+	appendAssembly(cg.currInstrs(), "B "+sndLabel, 1, 1)
 	appendAssembly(cg.currInstrs(), fstLabel+":", 0, 1)
 
 	cg.cgVisitScopeStat(Scope{StatList: node.DoStat})
