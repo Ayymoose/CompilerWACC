f6fc3a7ca5872404f486bf6c648ac9047af430f4
Henryk Hadass <hh2214@ic.ac.uk>
2015-12-08 03:13:56 +0000

    Modified MOD in binop register saving

--- src/backend/codeGeneration/codeGenWalker.go
+++ src/backend/codeGeneration/codeGenWalker.go
@@ -971,7 +971,7 @@ func (cg *CodeGenerator) cgVisitBinopExpr(node Binop) {
 		appendAssembly(cg.currInstrs(), "MOV r4, r0", 1, 1)
 		cg.cgVisitBinopExpr_H("p_check_divide_by_zero")
 	case MOD:
-		appendAssembly(cg.currInstrs(), "MOV r1, r5", 1, 1)
+		//	appendAssembly(cg.currInstrs(), "MOV r1, r5", 1, 1)
 		appendAssembly(cg.currInstrs(), "BL p_check_divide_by_zero", 1, 1)
 		appendAssembly(cg.currInstrs(), "BL __aeabi_idivmod", 1, 1)
 		appendAssembly(cg.currInstrs(), "MOV r0, r4", 1, 1)
